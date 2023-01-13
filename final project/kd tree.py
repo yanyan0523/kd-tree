bd88476cf70122d4f19cc693ae5f90068bc8d734
yanyan0523 <122452869+yanyan0523@users.noreply.github.com>
2023-01-13 11:30:58 +0800

    Update kd tree.py

--- final project/kd tree.py
+++ final project/kd tree.py
@@ -3,3 +3,65 @@ class Point)namedtuble('Point','x y'):
       return f'Point{tuple(self)!r}'
 
 class Rectangle(namestuple('Rectangle','lower upper')):
+  def__repr__(self)->str:
+     return f'Rectangle{tuple(self)!r}'
+
+  def is_contains(self,p:Point) -> bool:
+      return self.lower.x<=p.x<=self.upper.x and self.lower.y<=p.y<=self.upper.y
+
+class Node(namedtuple('Node','location left right')):
+'''Location:point,left:node, right:node'''
+  def__repr__(self):
+     return f'{tuple(self)!r}'
+
+class KDtree
+  def __init__(self):
+      self._root=None
+      self._n=0
+  
+  def insert(self,p:List[Point],depth=0):
+   #insert a list of points into the tree#
+      if not p:
+           return
+   #choose axis based on depth so that axis cycles through all valid values#
+      k= depth%2  # k=0 for x-axis and k=1 for y-axis #
+   #sort point list and choose median as pivot element#
+      p=sorted(p,key=lambda point:point[k])
+      median=len(p)//2
+   #if the selected median has other points that are the same as #
+   #it in the k-th dimension ,select the last one to satisfy the nature of the k-d tree#
+   #(left subtree<=current node< right subtree in the k-th dimension#
+      while median<len(p)-1 and p[median][k]==p[median+1][k]:
+          median=median+1
+
+   #current node'a point#
+   pivot =p[median]
+   #create node and recursively insert left and right subtree#
+   self._n+=1
+   node=Node(location=pivot,left=self.insert(p[:median],depth+1))
+   
+   #update the self_root#
+   if depth=0:
+       self._root=node
+   return node
+def range(self,rectangle:Rectangle,node=None, depth=0, points=None -> List[Point]:
+# Range query in the K-D tree#
+ if depth==0:
+     node= self_root
+     points=[]
+  
+ if node is None:
+    return points
+ # if the current node is in the rectangle add it to the list#
+ if rectangle.is_contains(node.location):
+    points.append(node.location)
+
+ k=depth%2 #k=o to x-axis,k=1 to y-axis#
+ if rectangle.lower[k]<=node.location[k]:
+    points =self.range(rectangle, node.left, depth+1 , points)
+ if rectangle.upper[k]>node.location[k]:
+    points=self.range(rectangle,node.right, depth+1, points)
+ return points
+
+
+
